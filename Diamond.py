df82e34e1a3965113d33d93401a407c91e650139
amirraeisi <amirmohamadraeisinasab@gmail.com>
2025-03-06 01:36:21 +0330

    shapes

---
+++ Diamond.py
@@ -0,0 +1,6 @@
+n=5
+for x in range(1 ,n+1):
+    print(' ' * (n-x) + '*' * (2 * x -1 ))
+    n=5
+for x in range(n ,0 , -1):
+    print(' ' * (n-x) + '*' * (2 * x -1 ))
---
+++ Equilateral triangle 2.py
@@ -0,0 +1,3 @@
+n=5
+for x in range(n ,0 , -1):
+    print(' ' * (n-x) + '*' * (2 * x -1 ))
